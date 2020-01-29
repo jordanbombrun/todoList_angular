577d3750038b9a375dd5270d5f2edc7637e2ca08
jordan b <bjordan.dev@outlook.com>
2020-01-29 11:39:46 +0100

    fonction pour supprimer des listItems à coder

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -9,12 +9,25 @@ import ListItem from '../models/listItem.model';
 })
 export class ListComponent implements OnInit {
 
+  newListItem = "";
+
   @Input()
   list: List;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  createListItem() {
+    this.list.listItems.push(new ListItem(this.newListItem));
+  }
+
+  editListItem() {
+  }
+
+  deleteListItem() {
+    
   }
 
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  username = "";
+  password = "";
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  createUser() {
+  }
+
+  cancelCreateUser() {
+    console.log(this.password);
   }
 
 }
